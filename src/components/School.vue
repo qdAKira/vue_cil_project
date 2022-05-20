5338f11265f97448cc7ca82b7fb6b269e838733b
jinjing <995538274@qq.com>
2022-05-20 16:58:39 +0800

    准备模块

---
+++ src/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+   <div>
+       <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showN">点我提示信息</button>
+    </div>
+</template>
+
+<script>
+export default {
+      name:'School',
+        data(){
+          return{
+            name:'sws',
+            address:'南通'
+          }
+        },
+        methods: {
+          showN(){
+            alert(this.name)
+          }
+        }
+}
+</script>
+
